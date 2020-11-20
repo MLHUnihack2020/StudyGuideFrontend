a43838de7a96c9b83788591c8d8958795f37ae24
Max Ratmeyer <17440617+MaxRatmeyer@users.noreply.github.com>
2020-11-20 16:57:39 -0500

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    <p class="text-lg font-bold">{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
