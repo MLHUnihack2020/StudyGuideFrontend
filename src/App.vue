4ab4ce888cc148a84249e199cbf2efaa6e9df2bb
Max Ratmeyer <17440617+MaxRatmeyer@users.noreply.github.com>
2020-11-20 17:52:31 -0500

    Added header and text box for submitting text

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Test"/>
+    <ContentSubmit />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContentSubmit from './components/ContentSubmit.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ContentSubmit
   }
 }
 </script>
---
+++ src/components/ContentSubmit.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">
+    <h1>Study Guide App</h1>
+    <p>Paste in your text that you want to study below:</p>
+
+    <textarea id="inputText" name="inputText"></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentSubmit'
+}
+</script>
