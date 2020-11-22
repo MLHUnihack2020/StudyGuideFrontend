666454cd7ecc7805864319bf7eb2b8e4619b0de3
Max Ratmeyer <17440617+MaxRatmeyer@users.noreply.github.com>
2020-11-21 22:24:29 -0500

    Buttonified flash card

--- src/components/FlashCard.vue
+++ src/components/FlashCard.vue
@@ -6,10 +6,10 @@
         <p class="text-center text-xl" v-if="cardSide == 1">{{ this.$parent.flashcards[cardNumber].answer }}</p>
       </div>
       <div class="">
-        <button class="font-bold p-5" v-on:click="previous">Previous</button>
-        <button class="font-bold p-5" v-on:click="flip">Flip</button>
-        <button class="font-bold p-5" v-on:click="next">Next</button>
         <button class="font-bold p-5" v-on:click="shuffle">Shuffle</button>
+        <button class="font-bold px-5 py-2 mr-2 shadow-md rounded-lg" v-on:click="previous">Previous</button>
+        <button class="font-bold px-5 py-2 mr-2 shadow-md rounded-lg" v-on:click="flip">Flip</button>
+        <button class="font-bold px-5 py-2 mr-2 shadow-md rounded-lg" v-on:click="next">Next</button>
         <button class="font-bold p-5" v-if="this.hintUsed === false && this.cardSide == 0" v-on:click="hint">Stuck? Get a hint.</button>
       </div>
     </div>
